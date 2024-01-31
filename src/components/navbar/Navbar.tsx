5c3dc7830e4ad67041aef02adfc040430f824f69
michele25-10 <michele.gabri01@gmail.com>
2024-01-31 16:02:16 +0100

    Creazione Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,22 @@ import "./navbar.scss";
 function Navbar() {
   return (
     <>
-      <div className="navbar">Navbar</div>
+      <div className="navbar">
+        <div className="logo">
+          <img src="logo.svg" alt="logo" />
+          <span>Money-Flow</span>
+        </div>
+        <div className="icons">
+          <div className="user">
+            <img
+              src="https://avatars.githubusercontent.com/u/72445614?v=4"
+              alt="img-profilo"
+            />
+            <span>Michele Gabrieli</span>
+          </div>
+          <div className="logout"></div>
+        </div>
+      </div>
     </>
   );
 }
