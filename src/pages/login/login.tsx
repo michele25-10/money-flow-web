926876fb170163447d3cf8d0a9d79a2127b36cd8
michele25-10 <michele.gabri01@gmail.com>
2024-02-03 22:18:09 +0100

    Terminato login, da aggiungere i controlli

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,10 +1,55 @@
+import { useNavigate } from "react-router-dom";
 import "./login.scss";
+
+const handleLogin = () => {
+  console.log("Login");
+  location.href = "http://localhost:5173/";
+};
 
 function Login() {
   return (
-    <>
-      <div className="home">Login</div>
-    </>
+    <div className="background">
+      <div className="containerLogin">
+        <div className="imgLogo">
+          <img
+            src="https://us.123rf.com/450wm/popcornarts/popcornarts2311/popcornarts231102334/218740987-asset-and-money-icon-concept.jpg?ver=6"
+            alt=""
+          />
+        </div>
+        <div className="formData">
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="cliente"
+              placeholder="Cliente..."
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="username"
+              className="form-control"
+              id="username"
+              aria-describedby="usernameHelp"
+              placeholder="Username..."
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Password..."
+            />
+          </div>
+          <div className="submit-button mt-3">
+            <button className="btn" onClick={() => handleLogin()}>
+              Accedi
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
