fbfaf1698a6a58393e00e61d5ec388861c5b708a
michele25-10 <michele.gabri01@gmail.com>
2024-02-04 15:23:10 +0100

    Terminata pagina dei log dell'applicazione

--- src/pages/log/Log.tsx
+++ src/pages/log/Log.tsx
@@ -1,10 +1,18 @@
+//COMPONENT
+import DataTable from "../../components/datatable/DataTable";
+import { columnsTable, rowsTable } from "./data";
+
+//SASS
 import "./log.scss";
 
 function Log() {
   return (
-    <>
-      <div className="home">Log</div>
-    </>
+    <div className="logContainer">
+      <div className="info">
+        <h1>Log</h1>
+      </div>
+      <DataTable columns={columnsTable} rows={rowsTable} action={false} />
+    </div>
   );
 }
 
