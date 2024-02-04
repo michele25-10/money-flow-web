6f0ef59c325c93bedf9c51c37d29fa63a1e7a776
michele25-10 <michele.gabri01@gmail.com>
2024-02-04 12:16:29 +0100

    Migliorata grafica login

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
 import "./login.scss";
+
+//ICON material ui
+import LockIcon from "@mui/icons-material/Lock";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import GroupIcon from "@mui/icons-material/Group";
+import PersonIcon from "@mui/icons-material/Person";
 
 const handleLogin = () => {
   console.log("Login");
@@ -6,6 +14,17 @@ const handleLogin = () => {
 };
 
 function Login() {
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className="background">
       <div className="containerLogin">
@@ -16,36 +35,64 @@ function Login() {
           />
         </div>
         <div className="formData">
-          <div className="mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="cliente"
-              placeholder="Cliente..."
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="username"
-              className="form-control"
-              id="username"
-              aria-describedby="usernameHelp"
-              placeholder="Username..."
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Password..."
-            />
-          </div>
-          <div className="submit-button mt-3">
-            <button className="btn" onClick={() => handleLogin()}>
-              Accedi
-            </button>
-          </div>
+          <form>
+            <div className="mb-3">
+              <div className="input-group mb-3">
+                <span className="input-group-text bg-white" id="basic-addon1">
+                  <GroupIcon />
+                </span>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="family"
+                  placeholder="Famiglia..."
+                  required
+                />
+              </div>
+            </div>
+            <div className="mb-3">
+              <div className="input-group mb-3">
+                <span className="input-group-text bg-white" id="basic-addon1">
+                  <PersonIcon />
+                </span>
+                <input
+                  type="username"
+                  className="form-control"
+                  id="username"
+                  aria-describedby="usernameHelp"
+                  placeholder="Username..."
+                  required
+                />
+              </div>
+            </div>
+            <div className="mb-3">
+              <div className="input-group mb-3">
+                <span className="input-group-text bg-white">
+                  <LockIcon />
+                </span>
+                <input
+                  type={showPassword ? "text" : "password"}
+                  value={password}
+                  onChange={handlePasswordChange}
+                  className="form-control"
+                  id="password"
+                  placeholder="Password..."
+                  required
+                />
+                <span
+                  className="input-group-text bg-white"
+                  onClick={handleTogglePassword}
+                >
+                  {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                </span>
+              </div>
+            </div>
+            <div className="submit-button mt-3">
+              <button className="btn" onClick={() => handleLogin()}>
+                Accedi
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
