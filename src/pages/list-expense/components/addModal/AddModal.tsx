9dafcea9047a7fce844ab3b7b6c396d3c45d31a6
michele25-10 <michele.gabri01@gmail.com>
2024-02-06 18:40:51 +0100

    Creato AddModel con anche il form al suo interno

--- src/pages/list-expense/components/addModal/AddModal.tsx
+++ src/pages/list-expense/components/addModal/AddModal.tsx
@@ -11,6 +11,12 @@ type Props = {
   edit: boolean;
 };
 
+const CarattereObbligatiorio = () => {
+  return (
+    <span style={{ fontSize: "16px", color: "red", fontWeight: "500" }}>*</span>
+  );
+};
+
 const AddModal = (props: Props) => {
   return (
     <div className="addModal">
@@ -28,18 +34,100 @@ const AddModal = (props: Props) => {
           </Modal.Title>
         </Modal.Header>
         <form>
-          <Modal.Body>Prova</Modal.Body>
+          <Modal.Body>
+            <div className="form">
+              <div className="inputLayer">
+                <label className="">
+                  Luogo:
+                  <CarattereObbligatiorio />
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  aria-describedby="Luogo"
+                  placeholder="Luogo..."
+                />
+              </div>
+              <div className="inputLayer">
+                <label className="">
+                  Data:
+                  <CarattereObbligatiorio />
+                </label>
+                <input
+                  type="date"
+                  className="form-control"
+                  aria-describedby="Data"
+                  placeholder="Data..."
+                />
+              </div>
+              <div className="inputLayer">
+                <label className="">
+                  Importo:
+                  <CarattereObbligatiorio />
+                </label>
+                <input
+                  type="number"
+                  className="form-control"
+                  aria-describedby="Importo"
+                  placeholder="Importo..."
+                />
+              </div>
+              <div className="inputLayer">
+                <label className="">Descrizione:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  aria-describedby="Descrizione"
+                  placeholder="Descrizione..."
+                />
+              </div>
+              <div className="inputLayer">
+                <label className="">
+                  Tipo Pagamento:
+                  <CarattereObbligatiorio />
+                </label>
+                <select className="form-select" aria-label="Tipo Pagamento">
+                  <option selected>Tipo Pagamento...</option>
+                  <option value="0">Contante</option>
+                  <option value="1">Bancomat</option>
+                </select>
+              </div>
+              <div className="inputLayer">
+                <label className="">
+                  Categoria:
+                  <CarattereObbligatiorio />
+                </label>
+                <select className="form-select" aria-label="Tipo Pagamento">
+                  <option selected>Categoria...</option>
+                  <option value="0">Amici</option>
+                  <option value="1">Fattura elettricità</option>
+                  <option value="2">Abbigliamento</option>
+                  <option value="3">Cibo</option>
+                  <option value="4">Carburante</option>
+                </select>
+              </div>
+              <div className="inputLayer">
+                <label className="">Documento:</label>
+                <input
+                  type="file"
+                  className="form-control"
+                  aria-describedby="Documento"
+                  placeholder="Documento..."
+                />
+              </div>
+            </div>
+          </Modal.Body>
           <Modal.Footer>
             <button
               style={{
                 width: "100px",
-                fontSize: "15px",
+                fontSize: "18px",
                 borderRadius: "10px",
                 borderColor: "orange",
-                height: "25px",
+
                 backgroundColor: "orange",
                 color: "white",
-                fontWeight: "500",
+                fontWeight: "400",
               }}
               onClick={() => {
                 props.setShow(false);
