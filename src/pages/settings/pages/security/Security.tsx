0e0c953806e39944cba40f888b05e09aea664290
michele25-10 <michele.gabri01@gmail.com>
2024-02-10 12:38:15 +0100

    Pagina di configurazione sicurezza

--- src/pages/settings/pages/security/Security.tsx
+++ src/pages/settings/pages/security/Security.tsx
@@ -1,5 +1,16 @@
+import "./security.scss";
+
 const Security = () => {
-  return <div>Sicurezza</div>;
+  return (
+    <div className="security">
+      <div className="title">
+        <h1>Sicurezza</h1>
+      </div>
+      <div className="messaggioSicurezza">
+        <h5 className=".avviso">Disponibile prossimamente</h5>
+      </div>
+    </div>
+  );
 };
 
 export default Security;
