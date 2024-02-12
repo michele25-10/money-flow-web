27384286ba69fdd188de6bcf4c32ca3437e2f90e
michele25-10 <michele.gabri01@gmail.com>
2024-02-12 08:40:48 +0100

    Terminata pagina cambio password

--- src/pages/settings/pages/changePassword/ChangePassword.tsx
+++ src/pages/settings/pages/changePassword/ChangePassword.tsx
@@ -22,57 +22,73 @@ const ChangePassword = () => {
         <h1>Cambio password</h1>
       </div>
       <div className="formCambioPassword">
-        <div className="inputLayer input-group mb-3">
-          <label className="label">Vecchia password:</label>
-          <input
-            type={showOldPassword ? "text" : "password"}
-            className="form-control"
-            id="oldPassword"
-            name="oldPassword"
-            placeholder="Vecchia password..."
-            required
-          />
-          <span
-            className="input-group-text bg-white opacity-75 text-decoration-none hover-pointer"
-            onClick={() => setShowOldPassword(!showOldPassword)}
-          >
-            {showOldPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
-          </span>
+        <div className="inputLayer input-group">
+          <label className="form-label">Vecchia password:</label>
+          <div className="input-group">
+            <input
+              type={showOldPassword ? "text" : "password"}
+              className="form-control"
+              id="oldPassword"
+              name="oldPassword"
+              placeholder="Vecchia password..."
+              required
+            />
+            <span
+              className="input-group-text"
+              id="basic-addon2"
+              onClick={() => setShowOldPassword(!showOldPassword)}
+            >
+              {showOldPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+            </span>
+          </div>
         </div>
-        <div className="input input-group mb-3">
-          <label className="label">Nuova password:</label>
-          <input
-            type={showNewPassword ? "text" : "password"}
-            className="form-control"
-            id="nuovaPassword"
-            name="newPassword"
-            placeholder="Nuova password..."
-            required
-          />
-          <span
-            className="input-group-text bg-white opacity-75 text-decoration-none hover-pointer"
-            onClick={() => setShowNewPassword(!showNewPassword)}
-          >
-            {showNewPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
-          </span>
+
+        <div className="inputLayer input-group">
+          <label className="form-label">Nuova password:</label>
+          <div className="input-group">
+            <input
+              type={showNewPassword ? "text" : "password"}
+              className="form-control"
+              id="nuovaPassword"
+              name="newPassword"
+              placeholder="Nuova password..."
+              required
+            />
+            <span
+              className="input-group-text"
+              id="basic-addon2"
+              onClick={() => setShowNewPassword(!showNewPassword)}
+            >
+              {showNewPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}{" "}
+            </span>
+          </div>
         </div>
-        <div className="input input-group mb-3">
-          <label className="label">Ripeti password:</label>
-          <input
-            type={showNewAgainPassword ? "text" : "password"}
-            className="form-control"
-            id="againPassword"
-            name="againPassword"
-            placeholder="Ripeti password..."
-            required
-          />
-          <span
-            className="input-group-text bg-white opacity-75 text-decoration-none hover-pointer"
-            onClick={() => setShowNewAgainPassword(!showNewAgainPassword)}
-          >
-            {showNewPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
-          </span>
+
+        <div className="inputLayer input-group">
+          <label className="form-label">Ripeti password:</label>
+          <div className="input-group">
+            <input
+              type={showNewAgainPassword ? "text" : "password"}
+              className="form-control"
+              id="againPassword"
+              name="againPassword"
+              placeholder="Ripeti password..."
+              required
+            />
+            <span
+              className="input-group-text"
+              id="basic-addon2"
+              onClick={() => setShowNewAgainPassword(!showNewAgainPassword)}
+            >
+              {showNewAgainPassword ? (
+                <VisibilityIcon />
+              ) : (
+                <VisibilityOffIcon />
+              )}{" "}
+            </span>
+          </div>
         </div>
+
         <button className="btn btn-warning" onClick={() => HandleSave()}>
           Salva
         </button>
