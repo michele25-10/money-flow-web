b21063e499c4b28de0a79c0244b7c5aa44bc002f
michele25-10 <michele.gabri01@gmail.com>
2024-02-13 16:01:04 +0100

    Pagina drive

--- src/pages/documentation/Documentation.tsx
+++ src/pages/documentation/Documentation.tsx
@@ -1,29 +1,80 @@
+//ICONS
 import SearchIcon from "@mui/icons-material/Search";
+import FolderIcon from "@mui/icons-material/Folder";
+import ArticleIcon from "@mui/icons-material/Article";
+import AddToDriveIcon from "@mui/icons-material/AddToDrive";
+
+//SASS
+import "./documentation.scss";
+
+//DATI
+import { docs } from "./data";
+
+type Doc = {
+  name: string;
+  type: string;
+};
 
 const Documentation = () => {
+  const styleIcons = {
+    color: "white",
+    height: "70px",
+    width: "70px",
+    cursor: "pointer",
+  };
+
   return (
     <div className="documentation">
-      <div className="headerform">
+      <div className="headerForm">
         <div className="title">
           <h1>Drive</h1>
         </div>
         <div className="searchForm">
-          <div className="mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="searchDoc"
-              placeholder="Cerca..."
+          <input
+            type="text"
+            className="form-control"
+            id="searchDoc"
+            placeholder="Cerca..."
+          />
+          <div className="icons">
+            <SearchIcon
+              style={{
+                height: "30px",
+                width: "30px",
+                cursor: "pointer",
+                color: "white",
+              }}
             />
           </div>
-          <SearchIcon
-            style={{
-              height: "25px",
-              width: "25px",
-              cursor: "pointer",
-              color: "white",
-            }}
-          />
+        </div>
+      </div>
+      <div className="containerDocs">
+        <div className="gridDocs">
+          {docs.map((item) => (
+            <div className="item">
+              <div className="iconDoc">
+                {item.type === "folder" ? (
+                  <FolderIcon style={styleIcons} />
+                ) : (
+                  <ArticleIcon style={styleIcons} />
+                )}
+              </div>
+              <div className="title">
+                <span>{item.name}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="addButton">
+          <button type="button" className="btn btn-success">
+            <AddToDriveIcon
+              style={{
+                height: "30px",
+                width: "30px",
+              }}
+            />
+          </button>
         </div>
       </div>
     </div>
---
+++ src/pages/documentation/data.ts
@@ -0,0 +1,26 @@
+export const docs = [
+  {
+    type: "folder",
+    name: "Spese familiari",
+  },
+  {
+    type: "pdf",
+    name: "Visita medica 2024",
+  },
+  {
+    type: "folder",
+    name: "Scuola",
+  },
+  {
+    type: "pdf",
+    name: "Cedolino Gennaio 2024",
+  },
+  {
+    type: "folder",
+    name: "Agenzia delle entrate",
+  },
+  {
+    type: "pdf",
+    name: "ISEE Michele Gabrieli 2024",
+  },
+];
