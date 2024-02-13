79b7c873563ba438e2b9a1c5477cad5889e059d3
michele25-10 <michele.gabri01@gmail.com>
2024-02-13 09:55:42 +0100

    terminata pagina spese fisse

--- src/pages/fixed-expense/FixedExpense.tsx
+++ src/pages/fixed-expense/FixedExpense.tsx
@@ -1,12 +1,16 @@
 //COMPONENT
-import TopBox from "../../components/topBox/TopBox";
+import GridBox from "../../components/gridBox/GridBox";
 import PieChartBox from "../../components/pieChartBox/PieChartBox";
 import BigChartBox from "../../components/bigChartBox/BigChartBox";
 
 //CSS
 import "./fixedExpense.scss";
 
-import { topBox, boxBar7, boxPie4 } from "./data";
+//icon
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+
+//data
+import { dataGridTable, gridColumnsTable, boxBar7, boxPie4 } from "./data";
 
 function FixedExpense() {
   return (
@@ -16,10 +20,38 @@ function FixedExpense() {
           <div className="title">
             <h1>Spese Fisse</h1>
           </div>
+          <div className="filterFormElement">
+            <div className="inputLayer">
+              <input
+                type="number"
+                min="1900"
+                max={new Date().getFullYear()}
+                step="1"
+                className="form-control"
+                id="exampleFormControlInput1"
+                placeholder="Anno"
+              />
+            </div>
+            <div className="span">
+              <FilterAltIcon
+                style={{
+                  color: "white",
+                  height: "30px",
+                  width: "30px",
+                  cursor: "pointer",
+                }}
+              />
+            </div>
+          </div>
         </div>
         <div className="gridFixedExpense">
           <div className="box tabellaSpese">
-            <TopBox title="Categoria" data={topBox} />
+            <GridBox
+              title="Ultime Spese"
+              action={false}
+              data={dataGridTable}
+              columns={gridColumnsTable}
+            />
           </div>
           <div className="box suddivisioneSpese">
             <PieChartBox {...boxPie4} />
--- src/pages/fixed-expense/data.ts
+++ src/pages/fixed-expense/data.ts
@@ -1,3 +1,5 @@
+import { GridColDef } from "@mui/x-data-grid";
+
 const pieData = [
   { name: "2023", value: 7500, color: "#8884d8" },
   { name: "2024", value: 8000, color: "#ff8042" },
@@ -66,6 +68,7 @@ const dataBigChart = [
 ];
 
 export const boxBar7 = {
+  title: "Andamento spese",
   chartData: dataBigChart,
   dataKey: [
     {
@@ -79,35 +82,42 @@ export const boxBar7 = {
   ],
 };
 
-export const topBox = [
+export const gridColumnsTable: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 60 },
+  {
+    field: "category",
+    headerName: "Categoria",
+    minWidth: 200,
+  },
+  {
+    field: "2023",
+    headerName: "2023",
+    type: "number",
+    minWidth: 200,
+  },
+  {
+    field: "2024",
+    headerName: "2024",
+    type: "number",
+    minWidth: 200,
+  },
+];
+
+export const dataGridTable = [
   {
     id: 1,
-    name: "Cibo",
-    tot: 3500,
+    category: "Fatture elettricità",
+    2023: 50,
+    2024: 20,
   },
-  {
-    id: 2,
-    name: "Fatture elettriche",
-    tot: 450,
-  },
-  {
-    id: 3,
-    name: "Amici",
-    tot: 200,
-  },
+  { id: 2, category: "Fatture Gas", 2023: 150, 2024: 125 },
+  { id: 3, category: "Contratti cellulari", 2023: 125, 2024: 159 },
   {
     id: 4,
-    name: "Mediche",
-    tot: 100,
+    category: "Rata auto",
+    2023: 4500,
+    2024: 7000,
   },
-  {
-    id: 5,
-    name: "Abbigliamento",
-    tot: 250,
-  },
-  {
-    id: 6,
-    name: "Carburante",
-    tot: 800,
-  },
+  { id: 5, category: "Mutuo", 2023: 1950, 2024: 2500 },
+  { id: 5, category: "TOTALE", 2023: 7500, 2024: 8000 },
 ];
